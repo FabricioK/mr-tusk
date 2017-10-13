bd7e4368bc4e5448cea84f1af5bd23122ba680bf
Fabricio S A <fabriciodossa@hotmail.com>
2017-10-13 00:23:33 -0300

    fixing packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,8 +19,25 @@ export class AppComponent implements AfterContentInit {
     var geometry = new THREE.BoxGeometry(1, 1, 1);
     var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     var cube = new THREE.Mesh(geometry, material);
-
-    scene.scene.add(cube);
+    var sceneSettings = {
+      element: document.body,
+      alpha: true,
+      antialias: true,
+      clearColor: '#fff',
+      sortObjects: false,
+      fog: null,
+      light: new THREE.DirectionalLight(0xffffff),
+      lightPosition: null,
+      cameraType: 'PerspectiveCamera',
+      cameraPosition: null, // {x, y, z}
+      orthoZoom: 4
+    }
+    if (!sceneSettings.lightPosition) {
+      sceneSettings.light.position.set(-1, 1, -1).normalize();
+    }
+    
+    scene.scene.add(sceneSettings.light);
+    scene.scene.add(new THREE.AmbientLight(0xdddddd));;
 
 
     this.engine.init({
@@ -28,7 +45,9 @@ export class AppComponent implements AfterContentInit {
       width: window.innerWidth,
       height: window.innerHeight
     });
+   
     this.engine.addScene('scence01', scene);
     this.engine.resumeScene();
+    scene.renderer.setClearColor('#fff',0);
   }
 }
